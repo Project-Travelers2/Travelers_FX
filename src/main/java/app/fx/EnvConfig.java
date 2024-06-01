8273915c7807e23bdfc83d5063d6d21104f598bf
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-06-02 07:22:59 +0900

    Env 테스트코드 작성

--- src/main/java/app/fx/EnvConfig.java
+++ src/main/java/app/fx/EnvConfig.java
@@ -10,7 +10,9 @@ public class EnvConfig {
     }
 
     public static void main(String[] args) {
-        System.out.println("API_KEY : " + getEnv("API_KEY"));
         System.out.println("DATABASE_URL: " + getEnv("DATABASE_URL"));
+        System.out.println("DATABASE_ID : " + getEnv("DATABASE_ID"));
+        System.out.println("DATABASE_PW : " + getEnv("DATABASE_PW"));
+        System.out.println("API_KEY : " + getEnv("API_KEY"));
     }
 }
