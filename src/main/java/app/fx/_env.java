d70a18591d4ff488de57c1a1b8bb06b5e558aaee
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-06-02 20:05:48 +0900

    Update _env.java
    환경설정 데이터 배치를 위해 env 클래스명 변경

--- src/main/java/app/fx/_env.java
+++ src/main/java/app/fx/_env.java
@@ -2,12 +2,19 @@ package app.fx;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
-public class EnvConfig {
+public class _env {
     private static final Dotenv dotenv = Dotenv.load();
 
     public static String getEnv(String key) {
         return dotenv.get(key);
     }
+
+    public static int pageNumber = 1;
+
+    /**
+     * pageNumber 초기화
+     */
+    public static void ResetPageNumber() { pageNumber = 1; }
 
     public static void main(String[] args) {
         System.out.println("DATABASE_URL: " + getEnv("DATABASE_URL"));
