b83fd35a9e53bf138c6a2295a0235930db5f7d0f
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-06-02 20:06:37 +0900

    env 파일 이름변경으로 리팩토링 진행

--- src/main/java/app/fx/HA/Queries.java
+++ src/main/java/app/fx/HA/Queries.java
@@ -1,11 +1,11 @@
 package app.fx.HA;
 
-import app.fx.EnvConfig;
+import app.fx.Data.FESTIVAL_INFORMATION;
+import app.fx._env;
 
 import java.sql.*;
-
-import app.fx.EnvConfig;
-import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Queries {
     public static final Queries instance = new Queries();
@@ -13,12 +13,12 @@ public class Queries {
     private String url, id, pw;
 
     private Queries() {
-        this.url = EnvConfig.getEnv("DATABASE_URL");
-        this.id = EnvConfig.getEnv("DATABASE_ID");
-        this.pw = EnvConfig.getEnv("DATABASE_PW");
+        this.url = _env.getEnv("DATABASE_URL");
+        this.id = _env.getEnv("DATABASE_ID");
+        this.pw = _env.getEnv("DATABASE_PW");
     }
 
-    public void all_festival_list() {
+    public List<FESTIVAL_INFORMATION> all_festival_list() {
         try (Connection conn = DriverManager.getConnection(url, id, pw)){
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery("select * from festival_information");
@@ -32,21 +32,38 @@ public class Queries {
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
+        return null;
     }
 
-    public void specific_festival_list(int festival_id) {
+    public List<FESTIVAL_INFORMATION> specific_festival_list(int festival_id) {
+        List<FESTIVAL_INFORMATION> festival_list = new ArrayList<>();
         try (Connection conn = DriverManager.getConnection(url, id, pw)){
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery("select * from festival_information where FESTIVAL_CODE = " + festival_id);
 
+//            List<FESTIVAL_INFORMATION> festival_list = new ArrayList<>();
             while(rs.next()) {
-                int f_num = rs.getInt("festival_mainid");
-                String f_name = rs.getString("festival_name");
-//                System.out.println(rs.getRow());
-                System.out.println(f_num+ "\t" +f_name);
+                FESTIVAL_INFORMATION f = new FESTIVAL_INFORMATION();
+
+                f.festival_loccode = rs.getInt("FESTIVAL_LOCCODE");
+                f.festival_code = rs.getInt("FESTIVAL_CODE");
+                f.festival_mainid = rs.getInt("FESTIVAL_MAINID");
+                f.festival_name = rs.getString("FESTIVAL_NAME");
+                f.location = rs.getString("LOCATION");
+                f.date_range = rs.getString("DATE_RANGE");
+                f.description = rs.getString("DESCRIPTION");
+                f.website_link = rs.getString("WEBSITE_LINK");
+
+                festival_list.add(f);
+//                System.out.println(f.toString());
             }
+
+//            return festival_list;
+
         } catch (SQLException ex) {
             ex.printStackTrace();
+        } finally {
+            return festival_list;
         }
     }
 }
