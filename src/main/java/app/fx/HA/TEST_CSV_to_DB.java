534b39ee8547d92ea58fc6a6fc31d41d429aa7cd
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-06-03 10:20:45 +0900

    Update TEST_CSV_to_DB.java
    중간 업데이트(에러 보정 필요)

--- src/main/java/app/fx/HA/TEST_CSV_to_DB.java
+++ src/main/java/app/fx/HA/TEST_CSV_to_DB.java
@@ -35,32 +35,57 @@ public class TEST_CSV_to_DB {
                     "(id, ident, name, latitude_deg, longitude_deg, elevation_ft, " +
                     "continent, iso_country, iso_region, municipality, scheduled_service," +
                     "gps_code, iata_code, local_code, home_link, wikipedia_link, keywords) " +
-                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
             PreparedStatement statement = connection.prepareStatement(sql);
 
             for (CSVRecord csvRecord : csvParser) {
-//                statement.setString(1, csvRecord.get("id"));
                 System.out.print(csvRecord.get("id") + "\t");
+                statement.setString(1, csvRecord.get("id"));
                 System.out.print(csvRecord.get("ident") + "\t");
+                statement.setString(2, csvRecord.get("ident"));
                 System.out.print(csvRecord.get("type") + "\t");
+                statement.setString(3, csvRecord.get("type"));
                 System.out.print(csvRecord.get("name") + "\t");
+                statement.setString(4, csvRecord.get("name"));
                 System.out.print(csvRecord.get("latitude_deg") + "\t");
+                statement.setString(5, csvRecord.get("latitude_deg"));
                 System.out.print(csvRecord.get("longitude_deg") + "\t");
+                statement.setString(6, csvRecord.get("longitude_deg"));
                 System.out.print(csvRecord.get("elevation_ft") + "\t");
+                statement.setString(7, csvRecord.get("elevation_ft"));
                 System.out.print(csvRecord.get("continent") + "\t");
+                statement.setString(8, csvRecord.get("continent"));
                 System.out.print(csvRecord.get("iso_country") + "\t");
+                statement.setString(9, csvRecord.get("iso_region") + "\t");
                 System.out.print(csvRecord.get("iso_region") + "\t");
+                statement.setString(10, csvRecord.get("municipality") + "\t");
                 System.out.print(csvRecord.get("municipality") + "\t");
+                statement.setString(11, csvRecord.get("scheduled_service") + "\t");
                 System.out.print(csvRecord.get("scheduled_service") + "\t");
+                statement.setString(12, csvRecord.get("gps_code") + "\t");
                 System.out.print(csvRecord.get("gps_code") + "\t");
+                statement.setString(13, csvRecord.get("keywords") + "\t");
                 System.out.print(csvRecord.get("iata_code") + "\t");
+                statement.setString(14, csvRecord.get("local_code") + "\t");
                 System.out.print(csvRecord.get("local_code") + "\t");
+                statement.setString(15, csvRecord.get("home_link") + "\t");
                 System.out.print(csvRecord.get("home_link") + "\t");
+                statement.setString(16, csvRecord.get("wikipedia_link") + "\t");
                 System.out.print(csvRecord.get("wikipedia_link") + "\t");
+                statement.setString(17, csvRecord.get("keywords") + "\t");
                 System.out.print(csvRecord.get("keywords") + "\t");
+                statement.setString(18, csvRecord.get("keywords") + "\t");
+                statement.addBatch();
+
                 System.out.println();
             }
+
+            statement.executeBatch();
+            connection.commit();
+
+            csvParser.close();
+            reader.close();
 
         } catch (SQLException | IOException e) {
             e.printStackTrace();
