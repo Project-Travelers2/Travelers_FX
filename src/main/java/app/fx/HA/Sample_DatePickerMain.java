a22366ca95da1195f7b68a7fe9ba29af995e4b73
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-06-03 19:16:50 +0900

    Create DatePicker sample

---
+++ src/main/java/app/fx/HA/Sample_DatePickerMain.java
@@ -0,0 +1,21 @@
+package app.fx.HA;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Sample_DatePickerMain extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("JavaFX DatePicker Example");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
