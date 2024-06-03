91bca7ede2781c72cb68e377ef0ffb6f1c5930ef
HyungJooHee <krlbt0128@gmail.com>
2024-06-03 14:24:24 +0900

    0603_

--- src/main/java/app/fx/HY/Festival.java
+++ src/main/java/app/fx/HY/Festival.java
@@ -1,5 +1,7 @@
 package app.fx.HY;
 
+import app.fx._env;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -11,10 +13,10 @@ public class Festival {
             Class.forName("oracle.jdbc.OracleDriver");
 
 
-            String url = "jdbc:oracle:thin:@localhost:1521/xe";
-            String sql = "select * from festival_information";
-            String id = "festival";
-            String pw = "oracle_4U";
+            String url = _env.getEnv("DATABASE_URL");
+            String sql = _env.getEnv("QUERY_ALL_INFO");
+            String id = _env.getEnv("DATABASE_ID");
+            String pw = _env.getEnv("DATABASE_PW");
             Connection conn = DriverManager.getConnection(url, id, pw);
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery(sql);
